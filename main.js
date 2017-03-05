9e59b1f5f4f744d224b59151627134d1f7ac7ad0
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-03-05 13:24:07 +0200

    initial

---
+++ main.js
@@ -0,0 +1,44 @@
+const bodyParser = require('body-parser')
+const EventEmitter = require('events');
+const express = require('express')
+const http = require('http')
+const jwt = require('jsonwebtoken')
+const url = require('url')
+const WebSocket = require('ws')
+const secret = process.env.JwtConfig__Secret || 'Put me to JwtConfig__Secret ENVIRONMENT variable'
+const app = express()
+const event = new EventEmitter();
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({
+	verifyClient: (info, done) => {
+		const { query: { access_token } } = url.parse(info.req.url, true)
+
+		if (!access_token) {
+			done(false)
+		} else {
+			jwt.verify(access_token.replace('Bearer ', ''), secret, (err, decoded) => done(err ? false : decoded))
+		}
+	},
+	server
+})
+
+wss.on('connection', ws => {
+	event.on('broadcast', body => {
+		if (ws.readyState === WebSocket.OPEN) {
+			ws.send(JSON.stringify(body))
+		}
+	})
+})
+
+app.post('/bitbucket', (req, res) => {
+	if (req.ip.indexOf('104.192.143') === 0) {
+		event.emit('broadcast', req.body)
+	}
+})
+
+server.listen(process.env.PORT || 3000)
